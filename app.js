1854b4efadc649ec0d4233f3faa655e0d53eb36d
Максим <maxdarma07@gmail.com>
2026-06-26 02:58:41 +0300

    add /login/, /promise/, /fetch/ routes with CORS

--- app.js
+++ app.js
@@ -4,8 +4,46 @@ const https = require('https');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/login', (req, res) => {
   res.send('amaenai1_');
+});
+
+app.get('/login/', (req, res) => {
+  res.send('amaenai1_');
+});
+
+app.get('/promise/', (req, res) => {
+  res.type('text/plain');
+  res.send('function task(x){ return new Promise((res, rej) => { if (x < 18) { res("yes"); } else { rej("no"); } }); }');
+});
+
+app.get('/fetch/', (req, res) => {
+  res.type('text/html; charset=UTF-8');
+  res.send(`<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Fetch page</title>
+</head>
+<body>
+<input id="inp">
+<button id="bt">Fetch</button>
+<script>
+document.getElementById('bt').addEventListener('click', () => {
+  const url = document.getElementById('inp').value;
+  fetch(url)
+    .then(response => response.text())
+    .then(data => { document.getElementById('inp').value = data; })
+    .catch(err => { document.getElementById('inp').value = err.message; });
+});
+</script>
+</body>
+</html>`);
 });
 
 app.get('/id/:n', (req, res) => {
